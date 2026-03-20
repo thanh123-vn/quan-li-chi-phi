340d7731e3015ee1b267b5e44df9297d6d1f8929
thanh123-vn <le716961@gmail.com>
2026-03-20 10:13:07 +0700

    fixx

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // để fix 404 Cloudflare
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
    eslint: {
     ignoreDuringBuilds: true,
   },
